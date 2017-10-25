5c5adbd99c9479da65091aa727656e63d3e955fe
Andrea <andrea@wittrock.us>
2017-10-24 23:45:06 -0500

    Finish implementing image selector upload functionality

--- src/components/media/selector/image.jsx
+++ src/components/media/selector/image.jsx
@@ -1,17 +1,24 @@
 
 import React from 'react';
-import {Row, Col, InputGroup, Button} from 'react-bootstrap';
+import {Row, Col, InputGroup, Button, Panel} from 'react-bootstrap';
 import NewMediaForm from '../../../containers/media/form/create';
+import {restUrl} from "atp-rest-client";
 
-export default props =>
+export default ({mediaId, image, loginToken, onSave}) =>
     <Row>
         <Col xs={12}>
-            <i className="fa fa-picture-o fa-5x"></i>
+            {mediaId && image &&
+                <Panel>
+                    <img src={restUrl("media/" + image.id + "/download?width=300&height=300&format=png&loginToken=" + loginToken)} className="img-responsive"/>
+                    <span>{image.name}</span>
+                </Panel>
+            }
+            {!mediaId && <i className="fa fa-picture-o fa-5x"></i>}
         </Col>
         <Col xs={12} className="text-right">
             <Button bsStyle="primary" bsSize="lg" type="submit" onClick={() => {alert("Not implemented yet. :(");}}>
                 <i className="fa fa-search" /> Search
             </Button>
-            <NewMediaForm onUpload={data => props.onSave(data.results.id)} />
+            <NewMediaForm onUpload={data => onSave(data.results.id)} />
         </Col>
     </Row>;
--- src/containers/media/selector/image.js
+++ src/containers/media/selector/image.js
@@ -2,14 +2,21 @@
  * Created by Andy on 10/24/2017.
  */
 
-import {connect} from 'react-redux';
+import {connectWithLifecycle} from "react-lifecycle-component";
 import ImageSelector from '../../../components/media/selector/image';
+import {Media} from "../../../reducer/media";
+import {getLoginToken} from 'atp-uac';
 
-export default connect(
+export default connectWithLifecycle(
     (state, props) => ({
-
+        image: props.mediaId ? Media().select.one(() => state, props.mediaId) : null,
+        loginToken: getLoginToken(() => state)
     }),
     (dispatch, props) => ({
-
+        componentDidMount: () => {
+            if(props.mediaId) {
+                dispatch(Media().action.fetch(props.mediaId));
+            }
+        }
     })
 )(ImageSelector);
