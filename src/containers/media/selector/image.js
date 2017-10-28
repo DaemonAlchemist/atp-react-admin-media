848aae07224ef789b2c27bc9c010efb68225f562
Andrea <andrea@wittrock.us>
2017-10-28 02:23:52 -0500

    Make sure arc images load correctly on both intial mount and subsequent updates

--- src/containers/media/selector/image.js
+++ src/containers/media/selector/image.js
@@ -2,21 +2,27 @@
  * Created by Andy on 10/24/2017.
  */
 
+import {connect} from 'react-redux';
 import {connectWithLifecycle} from "react-lifecycle-component";
 import ImageSelector from '../../../components/media/selector/image';
 import {Media} from "../../../reducer/media";
 import {getLoginToken} from 'atp-uac';
 
-export default connectWithLifecycle(
+const imageLoader = (props, dispatch) => () => {
+    if(props.mediaId && !props.image) {
+        dispatch(Media().action.fetch(props.mediaId));
+    }
+};
+
+export default connect(
     (state, props) => ({
         image: props.mediaId ? Media().select.one(() => state, props.mediaId) : null,
         loginToken: getLoginToken(() => state)
-    }),
-    (dispatch, props) => ({
-        componentDidMount: () => {
-            if(props.mediaId) {
-                dispatch(Media().action.fetch(props.mediaId));
-            }
-        }
     })
-)(ImageSelector);
+)(connectWithLifecycle(
+    null,
+    (dispatch, props) => ({
+        componentDidMount: imageLoader(props, dispatch),
+        componentDidUpdate: imageLoader(props, dispatch),
+    })
+)(ImageSelector));
