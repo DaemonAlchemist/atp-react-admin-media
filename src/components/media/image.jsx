1b5c430b081b4b6527558de0da8b6059d178b2eb
Andrea <andrea@wittrock.us>
2017-11-02 23:56:49 -0500

    Remove div from image component

--- src/components/media/image.jsx
+++ src/components/media/image.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {restUrl} from "atp-rest-client";
 
 export default ({imageId, width, height, loginToken}) =>
-    <div>
-        {imageId && <img src={restUrl(
+    imageId
+        ? <img src={restUrl(
                 "media/"
                 + imageId
                 + "/download?"
@@ -11,6 +11,6 @@ export default ({imageId, width, height, loginToken}) =>
                 + "&loginToken=" + loginToken
                 + (width ? "&width=" + width : "")
                 + (height ? "&height=" + height : "")
-            )} className="img-responsive"/>}
-        {!imageId && <i className="fa fa-picture-o fa-5x" />}
-    </div>
+            )} className="img-responsive"
+            />
+        : <i className="fa fa-picture-o fa-5x" />;
