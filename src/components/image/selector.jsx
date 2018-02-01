189acd29d3f697f3e355a8b2ef5382c062b9e00d
Andrew Wittrock <andy@wittrock.us>
2018-01-31 23:39:30 -0600

    Update for compatibility with react-bootstrap 0.32

--- src/components/image/selector.jsx
+++ src/components/image/selector.jsx
@@ -6,24 +6,27 @@ import Image from "../../containers/image";
 import {Icon} from 'react-font-awesome-5';
 
 export default ({title, imageId, image, loginToken, onSave, width, height}) =>
-    <Panel header={<span>
-        <Icon.Image /> {title}
-        <div style={{float: "right"}}>
-            <Button bsStyle="primary" bsSize="xsmall" type="submit" onClick={() => {alert("Not implemented yet. :(");}}>
-                <Icon.Search fixedWidth />
-            </Button>
-            <NewMediaForm bsSize="xsmall" showText={false} onUpload={data => onSave(data.results.id)} />
-        </div>
-    </span>}>
-        <Row>
-            <Col xs={12}>
-                {imageId && image &&
-                <div>
-                    <Image imageId={image.id} width={width} height={height} />
-                    <span>{image.name}</span>
-                </div>
-                }
-                {!imageId && <Icon.Image size="5x" />}
-            </Col>
-        </Row>
+    <Panel>
+        <Panel.Heading>
+            <Icon.Image /> {title}
+            <div style={{float: "right"}}>
+                <Button bsStyle="primary" bsSize="xsmall" type="submit" onClick={() => {alert("Not implemented yet. :(");}}>
+                    <Icon.Search fixedWidth />
+                </Button>
+                <NewMediaForm bsSize="xsmall" showText={false} onUpload={data => onSave(data.results.id)} />
+            </div>
+        </Panel.Heading>
+        <Panel.Body>
+            <Row>
+                <Col xs={12}>
+                    {imageId && image &&
+                    <div>
+                        <Image imageId={image.id} width={width} height={height} />
+                        <span>{image.name}</span>
+                    </div>
+                    }
+                    {!imageId && <Icon.Image size="5x" />}
+                </Col>
+            </Row>
+        </Panel.Body>
     </Panel>;
