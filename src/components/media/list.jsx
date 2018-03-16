decfc5967f7df9522027400f70f8b228a58e0ded
Andrew Wittrock <andy@wittrock.us>
2018-03-16 00:18:09 -0500

    Add pagination to media list

--- src/components/media/list.jsx
+++ src/components/media/list.jsx
@@ -1,19 +1,25 @@
 
 import React from "react";
-import {Row, Col, Panel} from "react-bootstrap";
+import {Row, Col, Panel, Button} from "react-bootstrap";
 import NewMediaForm from "../../containers/media/form/create";
 import Image from "../../containers/image";
 import Masonry from 'react-masonry-component';
 import LinesEllipsis from 'react-lines-ellipsis';
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC';
-import {DeleteButton} from 'atp-ui';
+import {DeleteButton, Paginator} from 'atp-ui';
+import {Icon} from 'react-font-awesome-5';
 
 const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 
-export default ({onUpload, media, onMediaDelete}) =>
+export default ({pagerId, onUpload, media, onMediaDelete}) =>
     <Row>
         <Col xs={12} className="text-right">
             <NewMediaForm showText={true} bsSize="lg" onUpload={onUpload} />
+        </Col>
+        <Col xs={12}>
+            <div style={{textAlign: "center"}}>
+                <Paginator name={pagerId} pagesToShow={{outer: 2, inner: 2}}/>
+            </div>
         </Col>
         <Col xs={12} style={{paddingTop: "15px"}}>
             <Row>
@@ -44,5 +50,10 @@ export default ({onUpload, media, onMediaDelete}) =>
                     )}
                 </Masonry>
             </Row>
+        </Col>
+        <Col xs={12}>
+            <div style={{textAlign: "center"}}>
+                <Paginator name={pagerId} pagesToShow={{outer: 2, inner: 2}}/>
+            </div>
         </Col>
     </Row>;
--- src/containers/media/list.js
+++ src/containers/media/list.js
@@ -2,17 +2,43 @@
 import {Media} from "../../reducer/media";
 import MediaList from "../../components/media/list";
 import {connectWithLifecycle} from 'react-lifecycle-component';
+import {paginator} from 'atp-ui';
+import {get} from 'atp-pointfree';
+
+const pagerId = "mediaListPager";
+const perPage = 10;
 
 export default connectWithLifecycle(
-    state => ({
-        media: Media().select.all(() => state, {}),
+    (state, props) => ({
+        pagerId,
+        offset: (paginator.get(get(state), pagerId).page - 1) * perPage,
+        perPage,
+        media: Media().select.byList(() => state, {
+            offset: (paginator.get(get(state), pagerId).page - 1) * perPage,
+            perPage
+        }),
     }),
-    (dispatch, getState) => ({
+    (dispatch, props) => ({
         componentDidMount: () => {
-            dispatch(Media().action.collection.get({}));
+            dispatch(Media().action.collection.get({
+                offset: 0,
+                perPage
+            }, data => {
+                dispatch(paginator.setPageCount(pagerId, Math.ceil(data.meta.totalRecords / perPage)));
+            }));
+        },
+        componentWillReceiveProps: function(newProps) {
+            if(newProps.offset !== this.props.offset) {
+                dispatch(Media().action.collection.get({
+                    offset: newProps.offset,
+                    perPage
+                }, data => {
+                    dispatch(paginator.setPageCount(pagerId, Math.ceil(data.meta.totalRecords / perPage)));
+                }));
+            }
         },
         onUpload: data => {
-            Media().action.collection.get({})(dispatch, getState);
+            Media().action.collection.get({})(dispatch, props);
             dispatch(Media().action.select(data.results.id));
         },
         onMediaDelete: id => () => {dispatch(Media().action.delete(id));}
