a87e650523ec89c46664185a04505ceb57be2966
Andrew Wittrock <andy@wittrock.us>
2018-07-28 15:27:46 -0500

    Add permission wrapper around new media form

--- src/components/media/form/create.jsx
+++ src/components/media/form/create.jsx
@@ -4,37 +4,40 @@ import {Button, InputGroup} from "react-bootstrap";
 import {Field} from "redux-form";
 import {FileInput} from 'atp-ui';
 import {Icon} from 'react-font-awesome-5';
+import {HasPermission} from "atp-uac";
 
 export default props =>
-    <form
-        onChange={() => setTimeout(props.handleSubmit, 0)}
-        style={{display: "inline", position: "relative"}}
-    >
-        <Field
-            name="file"
-            placeholder="Upload file"
-            component={FileInput}
-            style={{
-                position: "absolute",
-                top: "-9px",
-                left: 0,
-                display: "block",
-                height: "35px",
-                width: props.showText ? "84px" : "20px",
-                opacity: 0,
-                zIndex: 999
-            }}/>
-        <Button
-            bsStyle="link"
-            bsSize={props.bsSize || "regular"}
-            onClick={() => {}}
-            enabled={!props.isUploading}
-            style={{marginTop: "-4px"}}
+    <HasPermission permissions={["media.file.create"]}>
+        <form
+            onChange={() => setTimeout(props.handleSubmit, 0)}
+            style={{display: "inline", position: "relative"}}
         >
-            {props.isUploading
-                ? <Icon.Spinner spin fixedWidth />
-                : <Icon.Upload fixedWidth />
-            }
-            {props.showText ? " Upload" : ""}
-        </Button>
-    </form>;
+            <Field
+                name="file"
+                placeholder="Upload file"
+                component={FileInput}
+                style={{
+                    position: "absolute",
+                    top: "-9px",
+                    left: 0,
+                    display: "block",
+                    height: "35px",
+                    width: props.showText ? "84px" : "20px",
+                    opacity: 0,
+                    zIndex: 999
+                }}/>
+            <Button
+                bsStyle="link"
+                bsSize={props.bsSize || "regular"}
+                onClick={() => {}}
+                enabled={!props.isUploading}
+                style={{marginTop: "-4px"}}
+            >
+                {props.isUploading
+                    ? <Icon.Spinner spin fixedWidth />
+                    : <Icon.Upload fixedWidth />
+                }
+                {props.showText ? " Upload" : ""}
+            </Button>
+        </form>
+    </HasPermission>;
