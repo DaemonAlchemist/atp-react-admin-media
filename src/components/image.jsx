f7bdfefa1bab75f26947441ab60a085e031b690c
Andrew Wittrock <andy@wittrock.us>
2018-07-28 23:41:21 -0500

    Remove protocol from image urls

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -4,7 +4,7 @@ import {Icon} from 'react-font-awesome-5';
 export default ({image, resize, width, height, host}) =>
     image
         ? <img
-            src={`http://${host}/${image.s3Prefix ? image.s3Prefix + " - " : ""}${image.fileName}${resize}.${image.fileExtension}`}
+            src={`//${host}/${image.s3Prefix ? image.s3Prefix + " - " : ""}${image.fileName}${resize}.${image.fileExtension}`}
             className="img-responsive"
             width={width ? width : undefined}
             height={height ? height : undefined}
