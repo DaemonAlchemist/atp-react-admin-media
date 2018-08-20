3e44c6e4ff0d60abcb546ee93740fa27b77e5f4d
Andrew Wittrock <andy@wittrock.us>
2018-08-20 15:26:39 -0500

    Add basic media list filters

--- src/components/media/list.jsx
+++ src/components/media/list.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import {Row, Col, Panel, Button} from "react-bootstrap";
+import {Row, Col, Panel, InputGroup, Button, Badge} from "react-bootstrap";
 import NewMediaForm from "../../containers/media/form/create";
 import Image from "../../containers/image";
 import Masonry from 'react-masonry-component';
@@ -9,54 +9,78 @@ import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC';
 import {DeleteButton, Paginator} from 'atp-ui';
 import {Icon} from 'react-font-awesome-5';
 import {MediaLink} from "../links";
+import {TagSelector} from "atp-tags";
 
 const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 
-export default ({pagerId, onUpload, media, onMediaDelete}) =>
+export default ({pagerId, onUpload, media, onMediaDelete, onNameFilterChange, onAddTag, onRemoveTag, tagFilter}) =>
     <Row>
-        <Col xs={12} className="text-right">
-            <NewMediaForm showText={true} bsSize="lg" onUpload={onUpload} />
-        </Col>
-        <Col xs={12}>
-            <div style={{textAlign: "center"}}>
-                <Paginator name={pagerId} pagesToShow={{outer: 2, inner: 2}}/>
-            </div>
-        </Col>
-        <Col xs={12} style={{paddingTop: "15px"}}>
-            <Row>
-                <Masonry>
-                    {media.map(item =>
-                        <Col key={item.id} xs={6} sm={3} md={2}>
-                            <Panel>
-                                <Panel.Heading>
-                                    <div style={{float: "right"}}>
-                                        <DeleteButton
-                                            id={`mediaDeleteBtn${item.id}`}
-                                            text="Delete"
-                                            message={`Are you sure you want to delete the image "${item.fileName}.${item.fileExtension}?"  This cannot be undone.`}
-                                            confirmText="Yes, delete it"
-                                            cancelText="No, keep it."
-                                            width="250px"
-                                            onClick={onMediaDelete(item.id)}
-                                        />
-                                    </div>
-                                    <ResponsiveEllipsis
-                                        text={item.fileName + "." + item.fileExtension}
-                                        maxLine={2}
-                                    />
-                                </Panel.Heading>
-                                <MediaLink media={item}>
-                                    <Image imageId={item.id} width={300} height={300} />
-                                </MediaLink>
-                            </Panel>
-                        </Col>
+        <Col xs={12} sm={2}>
+            <Panel>
+                <Panel.Heading><Icon.Search /> Filter by file name:</Panel.Heading>
+                <Panel.Body>
+                    <input className="form-control" onChange={onNameFilterChange}/>
+                </Panel.Body>
+            </Panel>
+            <Panel>
+                <Panel.Heading><Icon.Search /> Filter by tags:</Panel.Heading>
+                <Panel.Body>
+                    {[...tagFilter.values()].map(tag =>
+                        <Badge>{tag}  <Icon.Times onClick={onRemoveTag(tag)}/></Badge>
                     )}
-                </Masonry>
-            </Row>
+                    <TagSelector id="mediaListTagFilter" onAdd={onAddTag} noCreate>
+                        <Icon.Plus /> Add tags
+                    </TagSelector>
+                </Panel.Body>
+            </Panel>
         </Col>
-        <Col xs={12}>
-            <div style={{textAlign: "center"}}>
-                <Paginator name={pagerId} pagesToShow={{outer: 2, inner: 2}}/>
-            </div>
+        <Col xs={12} sm={10}>
+            <Row>
+                <Col xs={12} className="text-right">
+                    <NewMediaForm showText={true} bsSize="lg" onUpload={onUpload} />
+                </Col>
+                <Col xs={12}>
+                    <div style={{textAlign: "center"}}>
+                        <Paginator name={pagerId} pagesToShow={{outer: 2, inner: 2}}/>
+                    </div>
+                </Col>
+                <Col xs={12} style={{paddingTop: "15px"}}>
+                    <Row>
+                        <Masonry>
+                            {media.map(item =>
+                                <Col key={item.id} xs={6} sm={3} md={2}>
+                                    <Panel>
+                                        <Panel.Heading>
+                                            <div style={{float: "right"}}>
+                                                <DeleteButton
+                                                    id={`mediaDeleteBtn${item.id}`}
+                                                    text="Delete"
+                                                    message={`Are you sure you want to delete the image "${item.fileName}.${item.fileExtension}?"  This cannot be undone.`}
+                                                    confirmText="Yes, delete it"
+                                                    cancelText="No, keep it."
+                                                    width="250px"
+                                                    onClick={onMediaDelete(item.id)}
+                                                />
+                                            </div>
+                                            <ResponsiveEllipsis
+                                                text={item.fileName + "." + item.fileExtension}
+                                                maxLine={2}
+                                            />
+                                        </Panel.Heading>
+                                        <MediaLink media={item}>
+                                            <Image imageId={item.id} width={300} height={300} />
+                                        </MediaLink>
+                                    </Panel>
+                                </Col>
+                            )}
+                        </Masonry>
+                    </Row>
+                </Col>
+                <Col xs={12}>
+                    <div style={{textAlign: "center"}}>
+                        <Paginator name={pagerId} pagesToShow={{outer: 2, inner: 2}}/>
+                    </div>
+                </Col>
+            </Row>
         </Col>
     </Row>;
--- src/containers/media/list.js
+++ src/containers/media/list.js
@@ -4,38 +4,77 @@ import MediaList from "../../components/media/list";
 import {connectWithLifecycle} from 'react-lifecycle-component';
 import {paginator} from 'basic-reducers';
 import {get} from 'atp-pointfree';
+import {input, set} from "basic-reducers";
 
 const pagerId = "mediaListPager";
 const perPage = 10;
+const filterId = "mediaListMainFilter";
+const fileNameFilterId = filterId + "FileName";
+const tagFilterId = filterId + "Tag";
+
+const fetchMedia = (dispatch, props, filters = {}) => {
+    dispatch(Media().action.collection.get({
+        ...filters,
+        offset: props.offset,
+        perPage
+    }, data => {
+        dispatch(paginator.setPageCount(pagerId, Math.ceil(data.meta.totalRecords / perPage)));
+    }));
+}
 
 export default connectWithLifecycle(
     (state, props) => ({
         pagerId,
         offset: (paginator.get(get(state), pagerId).page - 1) * perPage,
+        fileNameFilter: input.value(get(state), fileNameFilterId),
+        tagFilter: set.values(get(state), tagFilterId),
         perPage,
         media: Media().select.byList(() => state, {
             offset: (paginator.get(get(state), pagerId).page - 1) * perPage,
+            fileName: input.value(get(state), fileNameFilterId),
+            tag: [...set.values(get(state), tagFilterId).values()],
             perPage
         }),
     }),
     (dispatch, props) => ({
         componentDidMount: () => {
+            console.log("Mount");
             dispatch(Media().action.collection.get({
                 offset: 0,
+                fileName: null,
+                tag: [],
                 perPage
             }, data => {
                 dispatch(paginator.setPageCount(pagerId, Math.ceil(data.meta.totalRecords / perPage)));
             }));
         },
         componentWillReceiveProps: function(newProps) {
-            if(newProps.offset !== this.props.offset) {
+            if(
+                newProps.offset !== this.props.offset ||
+                newProps.fileNameFilter !== this.props.fileNameFilter ||
+                newProps.tagFilter !== this.props.tagFilter
+            ) {
+                console.log("Recieve props");
+                console.log(newProps);
                 dispatch(Media().action.collection.get({
                     offset: newProps.offset,
+                    fileName: newProps.fileNameFilter,
+                    tag: [...newProps.tagFilter.values()],
                     perPage
                 }, data => {
                     dispatch(paginator.setPageCount(pagerId, Math.ceil(data.meta.totalRecords / perPage)));
                 }));
             }
+        },
+        onNameFilterChange: e => {
+            dispatch(input.set(fileNameFilterId, e.target.value));
+        },
+        onAddTag: tag => {
+            console.log("Adding tag: " + tag);
+            dispatch(set.add(tagFilterId, tag));
+        },
+        onRemoveTag: tag => () => {
+            dispatch(set.remove(tagFilterId, tag));
         },
         onUpload: data => {
             Media().action.collection.get({})(dispatch, props);
